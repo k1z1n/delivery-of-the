74864208442068f1881e5a7f59dc2dab8fd2b085
k1z1n <martynm1314@gmail.com>
2024-07-31 22:24:35 +0300

    first commit

---
+++ resources/views/pages/main.blade.php
@@ -0,0 +1,25 @@
+@extends('includes.layout')
+@section('content')
+    <div class="flex items-center justify-center h-screen bg-gray-100">
+        <div class="max-w-xl bg-white shadow-md rounded-xl p-6">
+            <h4 class="text-xl font-semibold text-gray-800 mb-4">Проекты группы 23/20</h4>
+            <ul class="space-y-4">
+                <li class="text-blue-500 hover:underline">
+                    <a href="">Фамилия Имя Отчество</a>
+                </li>
+                <li class="text-blue-500 hover:underline">
+                    <a href="">Фамилия Имя Отчество</a>
+                </li>
+                <li class="text-blue-500 hover:underline">
+                    <a href="">Фамилия Имя Отчество</a>
+                </li>
+                <li class="text-blue-500 hover:underline">
+                    <a href="">Фамилия Имя Отчество</a>
+                </li>
+                <li class="text-blue-500 hover:underline">
+                    <a href="">Фамилия Имя Отчество</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+@endsection
